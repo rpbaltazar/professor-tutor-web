ba23102a51c0ff16c264458faaccfa425c5b30b1
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-01-30 12:48:36 +0800

    Added token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :first_name, :last_name
+
+  before_create do |user|
+    user.api_key = user.generate_api_key
+  end
+
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      break token unless User.exists?(api_key: token)
+    end
+  end
 end
