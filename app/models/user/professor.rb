b31a2a45098213b0ab7755a06a0b578fec53fdc9
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-10-28 17:13:25 +0800

    WIP - fixed controller structure and models sti

--- app/models/user/professor.rb
+++ app/models/user/professor.rb
@@ -1,11 +1,11 @@
-class Professor < User
+class User::Professor < User
   has_many :students, through: :tutorships
   has_many :tutorships
 
-  def find_user user_id
+  def find_user(user_id)
     begin
       student = students.find user_id
-    rescue ActiveRecord::RecordNotFound => e
+    rescue ActiveRecord::RecordNotFound
       student = nil
     end
     student
--- app/models/user/student.rb
+++ app/models/user/student.rb
@@ -1,13 +1,10 @@
-class Student < User
+class User::Student < User
   has_many :study_hours, dependent: :destroy, foreign_key: 'user_id'
   has_one :professor, through: :tutorship
   has_one :tutorship
 
-  def find_user user_id
-    if user_id == self.id
-      self
-    else
-      nil
-    end
+  def find_user(user_id)
+    return self if user_id == id
+    nil
   end
 end
