936887efa15406bca22243a36260bb278d30ed21
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-11-10 00:22:59 +0800

    WIP -

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BaseRequestOptions } from '@angular/http';
 import { AppComponent } from './app.component';
-
+import { routing }        from './app.routing';
+import { AuthGuard } from './_guards/index';
+import { AlertService, AuthenticationService } from './_services/index';
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    BaseRequestOptions],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
